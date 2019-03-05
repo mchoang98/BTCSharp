2cd760a2ddda4d49a418fe5e0aed4db5cf07f2de
mchoang98 <40058856+mchoang98@users.noreply.github.com>
2019-03-05 18:47:28 +0700

    Add project files.

---
+++ Thuc_Hanh_BT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Thuc_Hanh_BT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
